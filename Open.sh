96a015ef08cedc920e18a5c668171d052c80f4e7
Alex W <acwang@berkeley.edu>
2016-01-30 00:34:51 -0800

    rm .py added .sh

--- Open.sh
+++ Open.sh
@@ -3,15 +3,25 @@ __author__ = 'Alex'
 
 
 import webbrowser
+sites = [" 0) Google",
+		 " 1) MAIN CS61B",
+		 " 2) Github",
+		 " 3) CalCentral"]
+homework = [" q) PROJECT0  ::  due 1/29",
+			" w) MATH HW  ::  due 1/29"]
 
 def chrome(url):
 	webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s").open(url)
 print("Enter a url or enter a number of your top sites")
-print(" 0) Google")
-print(" 1) MAIN CS61B")
-print(" 2) PROJECT0  ::  due 1/29")
-print(" 3) ")
-
+print()
+for x in sites:
+	print(x)
+print()	
+print("- " * 20)
+print()
+for x in homework:
+	print(x)
+print("=" * 40)
 print()
 url = input("Enter a url" + "\n")
 
@@ -20,6 +30,12 @@ if url == "0":
 elif url == "1":
 	chrome("http://cs61b.ug/sp16/")
 elif url == "2":
+	chrome("https://github.com")
+elif url == "3":
+	chrome("https://calcentral.berkeley.edu")
+elif url == "q":
 	chrome("http://cs61b.ug/sp16/materials/proj/proj0/proj0.html")
+elif url == "w":
+	chrome("https://math.berkeley.edu/~apaulin/1Bhome1.pdf")	
 else:
 	chrome(url)
