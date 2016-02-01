d4417850a731201edbee454a4c566c1115dc98ff
Alex W <acwang@berkeley.edu>
2016-01-31 19:06:24 -0800

    Added a catch to non . inputs

--- Open.sh
+++ Open.sh
@@ -3,18 +3,25 @@ __author__ = 'Alex'
 
 
 import webbrowser
-sites = [" 0) Google",
+bookmark = [" 0) Google",
 		 " 1) MAIN CS61B",
 		 " 2) Github",
-		 " 3) CalCentral"]
-homework = [" q) PROJECT0  ::  due 1/29",
-			" w) MATH HW  ::  due 1/29"]
+		 " 3) CalCentral",
+		 " 4) IRC help"
+		]
+homework = [" q) ",
+			" w) "]
+sites = {0 : "http://google.com",
+		 1 : "ttp://cs61b.ug/sp16/",
+		 2 : "https://github.com",
+		 3 : "https://calcentral.berkeley.edu",
+		 }
 
 def chrome(url):
 	webbrowser.get("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s").open(url)
 print("Enter a url or enter a number of your top sites")
 print()
-for x in sites:
+for x in bookmark:
 	print(x)
 print()	
 print("- " * 20)
@@ -25,17 +32,37 @@ print("=" * 40)
 print()
 url = input("Enter a url" + "\n")
 
-if url == "0":
-	chrome("http://google.com")
-elif url == "1":
-	chrome("http://cs61b.ug/sp16/")
-elif url == "2":
-	chrome("https://github.com")
-elif url == "3":
-	chrome("https://calcentral.berkeley.edu")
-elif url == "q":
-	chrome("http://cs61b.ug/sp16/materials/proj/proj0/proj0.html")
-elif url == "w":
-	chrome("https://math.berkeley.edu/~apaulin/1Bhome1.pdf")	
-else:
-	chrome(url)
+# print(sites.keys())
+
+# if url in sites.keys():
+# # for item in sites:
+# 	chrome(sites[url])
+# 	return
+# elif len(url) == 1:
+# 	print("Not in dict")
+# 	return
+# else:
+# 	chrome(url)
+for x in url:
+	if x == ".":
+
+		if url == "0":
+			chrome("http://google.com")
+		elif url == "1":
+			chrome("http://cs61b.ug/sp16/")
+		elif url == "2":
+			chrome("https://github.com")
+		elif url == "3":
+			chrome("https://calcentral.berkeley.edu")
+		elif url == "4":
+			chrome("http://www.irchelp.org/irchelp/irctutorial.html")
+
+
+
+		elif url == "q":
+			chrome("http://cs61b.ug/sp16/materials/proj/proj0/proj0.html")
+		elif url == "w":
+			chrome("https://math.berkeley.edu/~apaulin/1Bhome1.pdf")	
+		else:
+			chrome(url)
+print("\n" + "~I can't go there~")
