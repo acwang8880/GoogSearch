c36ed2fe6244a95735f910ada57bd129d5cc8f8f
Alex W <acwang@berkeley.edu>
2016-03-21 14:33:18 -0700

    simplify dictionary structure

--- Open.sh
+++ Open.sh
@@ -12,9 +12,10 @@ bookmark = [" 0) Google",
 homework = [" q) ",
 			" w) "]
 sites = {0 : "http://google.com",
-		 1 : "ttp://cs61b.ug/sp16/",
+		 1 : "http://cs61b.ug/sp16/",
 		 2 : "https://github.com",
 		 3 : "https://calcentral.berkeley.edu",
+		 4 : "http://google.com"
 		 }
 
 def chrome(url):
@@ -32,37 +33,30 @@ print("=" * 40)
 print()
 url = input("Enter a url" + "\n")
 
-# print(sites.keys())
-
-# if url in sites.keys():
-# # for item in sites:
-# 	chrome(sites[url])
-# 	return
-# elif len(url) == 1:
-# 	print("Not in dict")
-# 	return
-# else:
-# 	chrome(url)
-for x in url:
-	if x == ".":
-
-		if url == "0":
-			chrome("http://google.com")
-		elif url == "1":
-			chrome("http://cs61b.ug/sp16/")
-		elif url == "2":
-			chrome("https://github.com")
-		elif url == "3":
-			chrome("https://calcentral.berkeley.edu")
-		elif url == "4":
-			chrome("http://www.irchelp.org/irchelp/irctutorial.html")
+print(sites.keys())
 
 
 
-		elif url == "q":
-			chrome("http://cs61b.ug/sp16/materials/proj/proj0/proj0.html")
-		elif url == "w":
-			chrome("https://math.berkeley.edu/~apaulin/1Bhome1.pdf")	
-		else:
-			chrome(url)
+if (url in sites.keys()):
+	sites[url]
+
+	# if x == "0":
+	# 	chrome("http://google.com")
+	# elif x == "1":
+	# 	chrome("http://cs61b.ug/sp16/")
+	# elif x == "2":
+	# 	chrome("https://github.com")
+	# elif x == "3":
+	# 	chrome("https://calcentral.berkeley.edu")
+	# elif x == "4":
+	# 	chrome("http://www.irchelp.org/irchelp/irctutorial.html")
+
+
+
+	# elif x == "q":
+	# 	chrome("http://cs61b.ug/sp16/materials/proj/proj0/proj0.html")
+	# elif x == "w":
+	# 	chrome("https://math.berkeley.edu/~apaulin/1Bhome1.pdf")	
+else: 
+	chrome(url)
 print("\n" + "~I can't go there~")
